9097fd3e1e05c925c6501924a3b6de1035351191
SAM-LD <slorenadiaz@gmail.com>
2026-08-08 00:34:28 -0300

    Install Meta OAuth error bridge

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from api import _bootstrap
 
 _bootstrap()
 
+from api.meta_error_bridge import install_meta_error_bridge
+install_meta_error_bridge()
+
 from api.index import app
 from api.privacy import router as privacy_router
 from api.meta_manual import router as meta_manual_router
