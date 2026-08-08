03c749d9fbdf62644bedbc333ef1b1b77ceb6b9b
SAM-LD <slorenadiaz@gmail.com>
2026-08-08 00:33:37 -0300

    Expose temporary manual Meta connector

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ _bootstrap()
 
 from api.index import app
 from api.privacy import router as privacy_router
+from api.meta_manual import router as meta_manual_router
 
 app.include_router(privacy_router)
+app.include_router(meta_manual_router)
 
 __all__ = ["app"]
